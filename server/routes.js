348809aa75b69f0bc7d8d717dec7738917808200
Raj Desai <rajdesai94@gmail.com>
2017-03-30 18:47:00 -0400

    Deleted dummy code

--- server/routes.js
+++ server/routes.js
@@ -1,20 +1,9 @@
 const path = require('path');
-// const controller = require('./api/controller.js');
 
 module.exports = function(app, express){
   const router = express.Router();
 
   //api endpoints and routes
-
-  // router.route('/characters')
-  //   .post(controller.createCharacter)
-  //   .get(controller.getCharacters);
-  //
-  // // Specific Character :id
-  // router.route('/characters/:id')
-  //   .put(controller.updateCharacter)
-  //   .get(controller.getCharacter);
-  // //   .delete(controller.deleteCharacter);
 
   app.use('/api', router);
 
